ebf98082f4ea5dd9a0c77663ce6d96e7ffb3e16e
brahim <brahimgb1team@gmail.com>
2022-02-07 13:45:51 +0000

    add css file assign variables in js

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body');
+const bklist = document.querySelector('.bklist');
+const bkTitle = document.getElementById('title');
+const bkAuthor = document.getElementById('author');
+const form = document.getElementById('form');
+let id = 0;
+const books = []; 
