f0c9445c53d88f03e271a47a28141237d6ae773b
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-09 12:43:13 +0300

    Update class refactor setup

---
+++ class.js
@@ -0,0 +1,71 @@
+const bklist = document.querySelector('.bookswrapper');
+const bkTitle = document.getElementById('title');
+const bkAuthor = document.getElementById('author');
+const addBtn = document.querySelector('.add-btn');
+
+class Library {
+  constructor() {
+    this.books = [
+      {
+        title: 'Manger',
+        author: 'Cuisinier',
+      },
+      {
+        title: 'Baron',
+        author: 'Fredrick',
+      }
+    ];
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.setLocalStorage();
+    this.displayBooks();
+  }
+
+  setLocalStorage() {
+    localStorage.setItem('Data-base', JSON.stringify(this.books));
+  }
+
+  getLocalStorage() {
+    if (localStorage.getItem('Data-base')) {
+      this.books = JSON.parse(localStorage.getItem('Data-base'));
+    } else {
+      this.books =  this.books;
+    }
+    this.displayBooks();
+  }
+
+  addBook(title, author) {
+    this.books.push({
+      title: title,
+      author: author,
+    });
+
+    bkAuthor.value = '';
+    bkTitle.value = '';
+    this.setLocalStorage();
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    bklist.innerHTML = '';
+    this.books.forEach((book, index)  => {
+    const li = document.createElement('li');
+    const description = document.createElement('p');
+
+    const btnRemove = document.createElement('button');
+    description.textContent = `"${book.title}" by ${book.author}`;
+    btnRemove.textContent = 'remove';
+
+
+    li.append(description, btnRemove);
+    btnRemove.addEventListener('click', () => {
+      this.removeBook(index);
+    })
+    bklist.appendChild(li);
+
+    })
+  }
+
+}
